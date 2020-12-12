a9ac1703159a3b6fa923efd37ce2c78981b0927a
amrutsabale <samrut355@gmail.com>
2020-12-12 18:23:04 +0530

    Updated Css changes

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function Login() {
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo" />
-            <a href={loginUri}>LOGIN WITH SPOTIFY</a>
+            <div className="login_button"> <a href={loginUri}>LOGIN WITH SPOTIFY</a></div>
         </div>
     )
 }
